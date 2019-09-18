7311fa7b95268ac9f6aba1fe1aca794bc3471d43
adeliadanjou <princessadelia@gmail.com>
2019-09-18 09:16:07 +0200

    iteration 1 done: service AccessControl

---
+++ starter-code/src/app/services/access-control-log.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AccessControlLogService {
+  
+  logMessages: Object[];
+
+  constructor() { }
+
+  addAccessItem(person, message){
+    var accessObject = {person, message, createdAt: new Date()}
+    this.logMessages.push(accessObject)
+  }
+
+  getAccesLog() {
+    return this.logMessages
+  }
+}
